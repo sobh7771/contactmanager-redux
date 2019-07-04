c6ea9e37079d3fd7e8c9f71df5b19b840c9542c0
ahmed <sobh6081@gmail.com>
2019-07-04 20:14:23 +0200

    build static

---
+++ build/precache-manifest.cb646ec4306f757b83183007018cd61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf78cc53109338c00beff94e7e3603a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0627c8b3b5e0ed63a767",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "199f535492d1c8679bf9",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0627c8b3b5e0ed63a767",
+    "url": "/static/js/2.5acf16c4.chunk.js"
+  },
+  {
+    "revision": "199f535492d1c8679bf9",
+    "url": "/static/js/main.be282905.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
